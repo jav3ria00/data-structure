442cb58db7f28232d421722c8de5c954447eab1e
jav3ria00 <126039440+jav3ria00@users.noreply.github.com>
2023-04-30 08:59:23 +0500

    Add files via upload

---
+++ Stack using array.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+using namespace std;
+
+int stack[100],n=100,top=-1;
+void push(int val)
+{
+	if(top>=n-1)
+	cout<<"Stack overflow"<<endl;
+	else
+	{
+		top++;
+		stack[top]=val;
+	}
+	}	
+	void pop()
+	{
+		if(top<=-1)
+		cout<<"Stack overflow"<<endl;
+		else{
+			cout<<"Pop element is "<<stack[top]<<endl;
+			top--;
+			
+		}
+	}
+	void display()
+	{
+		if(top>=0)
+		{
+			cout<<"Stack elemnets are";
+			for(int i=top;i>=0;i--)
+			cout<<stack[i]<<" ";
+			cout<<endl;
+			
+		}
+	cout<<"Stack empty";
+	
+	}
+	int main()
+	
+	{
+		int ch,val;
+		cout<<"1) Push  in stack"<<endl;
+			cout<<"2) Pop in stack"<<endl;
+		cout<<"3) Display stack"<<endl;		
+			cout<<"4)exit"<<endl;	
+		do{
+			cout<<"Enter choice:"<<endl;
+			cin>>ch;
+			switch(ch)
+			{
+				case 1:
+					
+					{
+						cout<<"Enter value to be pushed"<<endl;
+						cin>>val;
+						push(val);
+						break;
+					}
+					case 2:
+						{
+							pop();
+							break;
+						}
+					case 3:
+						{
+							display();
+							break;
+						}	
+						case 4:
+						{
+							cout<<"Exit"<<endl;;
+							break;
+						}			
+						default:{
+							
+						cout<<"Invalid Choice"<<endl;
+						}
+			
+			}
+		}
+while(ch!=4);
+return 0;
+	}
+
+
